39e3d0bd3c1ae1c677f28a80aa76040d96ec3841
ZekVoid <carlojamir91401@gmail.com>
2024-02-16 14:14:21 +0800

    Updated PDFviewer

--- frontend/src/views/PdfViewer.vue
+++ frontend/src/views/PdfViewer.vue
@@ -1,51 +1,61 @@
 <template>
-    <div class="pdfviewer">
-  
+  <div class="pdfviewer">
+    <div class="form-container" ref="formContainer">
       <label for="formFileLg" class="form-label">Upload File</label>
       <input class="form-control form-control-lg" id="formFileLg" type="file" ref="fileInput">
-  
-      <div class="form-floating">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-        <label for="floatingTextarea2">Comments</label>
-      </div>
-  
       <button id="btnView" type="button" class="btn btn-primary" @click="openPdf">
         View PDF
       </button>
-  
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      openPdf() {
-        const file = this.$refs.fileInput.files[0];
-  
-        if (file) {
-          const fileUrl = URL.createObjectURL(file);
-  
-          window.open(fileUrl, '_blank');
-        } else {
-          alert('Please select a PDF file to view.');
-        }
+    <iframe id="pdfViewer" width="100%" height="100%" ref="pdfViewer" style="position: fixed; right: 0; top: 70px; bottom: 20px; width: calc(100% - 800px); z-index: 1;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    openPdf() {
+      const file = this.$refs.fileInput.files[0];
+      const pdfViewer = this.$refs.pdfViewer;
+
+      if (file) {
+        const fileUrl = URL.createObjectURL(file);
+        pdfViewer.src = fileUrl;
+        pdfViewer.style.display = 'block';
+      } else {
+        alert('Please select a PDF file to view.');
       }
     }
-  };
-  </script>
-  
-  
-  <style scoped>
-
-  .form-floating {
-
-    margin-top: 25px;
   }
+};
 
-  #btnView {
+</script>
 
-    margin-top: 25px;
-  }
- 
-  </style>
-  
+<style scoped>
+.pdfviewer {
+  position: relative;
+  height: 100%; 
+}
+
+.form-container {
+  position: fixed;
+  left: 0;
+  top: 150px;
+  bottom: 20px;
+  width: 800px;
+  padding: 20px;
+
+  z-index: 1; 
+}
+
+#btnView {
+  margin-top: 15px;
+}
+
+.form-label {
+
+  font-size: 30px;
+}
+
+</style>
